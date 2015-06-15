0fd23d857a50a8015cf913d916d0dd84bd69a0a9
uzmanj <uzman.jacob@gmail.com>
2015-06-14 22:34:36 -0400

    skeleton project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,11 +64,15 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find_by(params[:name])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :password)
     end
+
+
+
+
 end
